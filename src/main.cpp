b39fc33f7379d86cb0e5dd2e7e8eea692a94c6ec
MisterMartin <1703358+MisterMartin@users.noreply.github.com>
2026-06-08 07:47:08 -0700

    Correct main.cpp for function sig change

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,15 @@
 RPUComm rpu(&Serial1);
 
 void setup() {
-    int8_t  i1, i2, i3;
+    int8_t  i1, i2, i3, i4;
     int32_t l1, l2;
     uint8_t u1;
     uint32_t u3;
     float f1, f2, f3, f4, f5;
     char buf[64];
 
-    rpu.TX_GoMeasure(0, 0, 0, 0, 0);
-    rpu.RX_GoMeasure(&l1, &l2, &i1, &i2, &i3);
+    rpu.TX_GoMeasure(0, 0, 0, 0, 0, 0);
+    rpu.RX_GoMeasure(&l1, &l2, &i1, &i2, &i3, &i4);
 
     rpu.TX_Status(0, 0.0f, 0.0f, 0.0f, 0.0f, 0);
     rpu.RX_Status(&u3, &f1, &f2, &f3, &f4, &u1);
