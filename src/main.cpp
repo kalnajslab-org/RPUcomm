ac4b2b7e16f63d38ed5a71ec3bb6bf460c7ce4fb
MisterMartin <1703358+MisterMartin@users.noreply.github.com>
2026-06-09 11:16:51 -0700

    Fix parameter types in TX_GoMeasure() and RX_GoMeasure() calls

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,8 @@ void setup() {
     float f1, f2, f3, f4, f5;
     char buf[64];
 
-    rpu.TX_GoMeasure(0, 0, 0, 0, 0, 0);
-    rpu.RX_GoMeasure(&l1, &l2, &i1, &i2, &i3, &i4);
+    rpu.TX_GoMeasure(0, 0, 0.0f, 0, 0, 0, 0);
+    rpu.RX_GoMeasure(&l1, &l2, &f1, &i1, &i2, &i3, &i4);
 
     rpu.TX_Status(0, 0.0f, 0.0f, 0.0f, 0.0f, 0);
     rpu.RX_Status(&u3, &f1, &f2, &f3, &f4, &u1);
