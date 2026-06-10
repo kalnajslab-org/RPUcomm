13f7f01cca1b0c2b96f6caac0fb71d325ec6abe5
MisterMartin <1703358+MisterMartin@users.noreply.github.com>
2026-06-09 20:49:31 -0700

    Fix the rpu com test compile for the new status api

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,14 @@ RPUComm rpu(&Serial1);
 void setup() {
     int8_t  i1, i2, i3, i4;
     int32_t l1, l2;
-    uint8_t u1;
-    uint32_t u3;
-    float f1, f2, f3, f4, f5;
+    float f1;
     char buf[64];
 
     rpu.TX_GoMeasure(0, 0, 0.0f, 0, 0, 0, 0);
     rpu.RX_GoMeasure(&l1, &l2, &f1, &i1, &i2, &i3, &i4);
 
-    rpu.TX_Status(0, 0.0f, 0.0f, 0.0f, 0.0f, 0);
-    rpu.RX_Status(&u3, &f1, &f2, &f3, &f4, &u1);
+    rpu.TX_Status("{}");
+    rpu.RX_Status(buf, sizeof(buf));
 
     rpu.TX_Error("test");
     rpu.RX_Error(buf, sizeof(buf));
