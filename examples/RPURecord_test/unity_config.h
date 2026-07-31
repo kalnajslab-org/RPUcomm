7728f61a4f36e63ad63fc2f34167f6acf47cb4c0
MisterMartin <1703358+MisterMartin@users.noreply.github.com>
2026-07-31 14:26:41 -0600

    Enable double in unity_config.h

--- examples/RPURecord_test/unity_config.h
+++ examples/RPURecord_test/unity_config.h
@@ -3,3 +3,4 @@ void unity_output_char(char c);
 void unity_output_flush(void);
 #define UNITY_OUTPUT_CHAR(a)  unity_output_char(a)
 #define UNITY_OUTPUT_FLUSH()  unity_output_flush()
+#define UNITY_INCLUDE_DOUBLE
